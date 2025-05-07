f8fe2da0f2d4f70e90ee092dcf7dac4d9aa399aa
Aurelio110 <daurys@outlook.de>
2025-05-07 11:10:47 +0200

    Add files via upload

---
+++ Weihnachtsmarkt/src/staende/Gluehweinstand.java
@@ -0,0 +1,20 @@
+package src.staende;
+
+public class Gluehweinstand extends Stand{
+
+
+    public Gluehweinstand(Bauart bauart, int groesse){
+        super(bauart, groesse);
+    }
+
+    @Override
+    public int mietkostenProTag() {
+        return 100 * groesse;
+    }
+
+    @Override
+    public boolean erfuelltAuflagen() {
+        return true;
+    }
+
+}
